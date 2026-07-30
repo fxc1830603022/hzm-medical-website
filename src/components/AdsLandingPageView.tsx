ff9d6a969f79db87c073bc078d441d1703794d77
fxc1830603022 <1830603022@qq.com>
2026-07-30 18:57:42 +0800

    Update Facebook Ads WhatsApp number

--- src/components/AdsLandingPageView.tsx
+++ src/components/AdsLandingPageView.tsx
@@ -89,7 +89,7 @@ const assessmentInstruction =
 const facebookMethodVideoSrc = "/videos/dr-xiao-9d-methodology-mobile-v2.mp4";
 const facebookArrivalVideoSrc = "/videos/facebook-arrival-support-mobile-v2.mp4";
 const facebookArrivalVideoPoster = "/videos/facebook-arrival-support-v21-poster.jpg";
-const facebookAdsWhatsAppNumber = "+17023035859";
+const facebookAdsWhatsAppNumber = "+13043567880";
 const googleAdsWhatsAppNumber = "+601121706171";
 const facebookResultImageOverrides = [
   "/images/facebook-ads-result-case-01-20260724.jpg",
